f1f95ba03195937baeff927d23968d8041fa9ac9
DESKTOP-RAA1R83\gkdyd <gkdyddl33@gmail.com>
2021-01-20 20:59:19 +0900

    [Update] 웹서버 연결해보기 / UI컴포넌트 위젯에 대해 알아보기

---
+++ RestProject/src/main/java/com/koreait/restproject/model/domain/Member.java
@@ -0,0 +1,14 @@
+package com.koreait.restproject.model.domain;
+
+import lombok.Data;
+
+@Data
+public class Member {
+	private int member_id;
+	private String m_id;
+	private String m_pass;
+	private String m_name;
+	public int getMember_id() {
+		return member_id;
+	}
+}
